23539e714a6ce00076a808dc9c4db79981b47f31
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-08 00:47:25 +0530

    Add files via upload

---
+++ variables/create.py
@@ -0,0 +1,17 @@
+# variables don't need a declaration in python
+
+x = 4 
+print(x)
+
+#double or single quotation marks don't matter
+#it is case sensitive
+
+y = 'aarushi' 
+Y = "mahajan"
+print(y)
+print(Y)
+
+# to specify the data type of any variable 
+
+z = int(40)
+print(type(z))
