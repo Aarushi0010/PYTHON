4de4ce080f13b5f92546f7d30e951a357135cfae
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-09 18:18:37 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,12 @@
+#i t means we are specifying the data type on to a variable 
+# Python is an object-oriented programming language , it uses classes 
+# to define data types including primitive data types 
+
+# it uses 3 constructor functions : int() , float() , str()
+
+x = int(4.5)
+print(x)
+y = float("44")
+print(y)
+z = str('s2')
+print(z)
