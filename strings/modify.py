65d740ce5c1c047a2a4070b58e0c3946069921f0
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-10 23:49:47 +0530

    Add files via upload

---
+++ strings/modify.py
@@ -0,0 +1,22 @@
+# To print string in upper case 
+
+a = "hello world!"
+print (a.upper())
+
+# To print string in lower case
+
+a = "HELLO WORLD!"
+print(a.lower())
+
+# To remove white spaces in the string 
+
+a = " Hello World !"
+print(a.strip())
+
+# Replace string 
+a = "Hello World!"
+print( a.replace("H" , "J"))
+
+# Split string 
+a = "hello , World"
+print(a.split(","))
