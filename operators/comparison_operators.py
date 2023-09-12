3283eddf90f41ab864473240320e85c3c417e7c2
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-13 01:35:29 +0530

    Add files via upload

---
+++ operators/comparison_operators.py
@@ -0,0 +1,22 @@
+# equal operator 
+
+x = 20
+y = 30
+
+print(x==y)
+
+# not equal operator
+
+print(x != y)
+
+# greater than operator
+print(x>y)
+
+# less than operator
+print(x<y)
+
+# greater than equal operator
+print(x>=y)
+
+# less than equal operator
+print(x<=y)
