8b80ff38840cf0c0cc942b6f53c8c92f6ed0a5a4
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-12 20:40:36 +0530

    Add files via upload

---
+++ operators/arithematic_operators.py
@@ -0,0 +1,39 @@
+# ADDITION OPERATORS 
+
+x = 10
+y = 20
+print ( x + y )
+
+# SUBTRACTION OPERTATORS
+
+print( y - x )
+
+# MULTIPLICATION OPERATORS
+print( x * y )
+
+# DIVISION OPERATORS
+# returns the quotient from division 
+
+x = 400
+y = 25
+print ( x/y )
+
+# FLOOR DIVISION 
+# it rounds the result to the nearest whole number
+
+x = 389
+y = 16
+print ( x// y )
+
+# MODULUS OPERATORS
+# returns the remainder from division
+
+x = 17
+y = 2
+print( x%y )
+
+# EXPONENTIATION OPERATORS 
+
+x = 5
+y = 3
+print( x**y )
