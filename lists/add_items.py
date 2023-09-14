4b53776e09cd3fc8268147c4c7f565caa64d1069
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-14 22:44:52 +0530

    Add files via upload

---
+++ lists/add_items.py
@@ -0,0 +1,14 @@
+# using append() method
+list = ["apple" , "mango" , "cherry"]
+list.append("melon")
+print(list)
+
+# using insert() method 
+list.insert(2 , "strawberry")
+print(list)
+
+# using extend() method
+pet_list = ["dog" , "cat" , "rabbit"]
+wild_list = ["elephant" , "tiger"]
+wild_list.extend(pet_list)
+print(wild_list)
