1fb235a96f7879e3f975a23365ebaf4396ca6d63
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-14 22:36:21 +0530

    Add files via upload

---
+++ lists/change_list.py
@@ -0,0 +1,17 @@
+# change item value 
+list = ["apple" , "orange" , "banana"]
+list[1] = "watermelon"
+print(list)
+
+# change range of item values
+list = [1 , 2 ,3 ,4 ,5, 6 ,7]
+list[2:5] = [10 , 11 , 12]
+print(list)
+
+# replace second item with two new items 
+list[1:2] = ["apple" , "watermelon"]
+print(list)
+
+# replace 2nd and 3rd item with one item 
+list[1:3] = ["sugar"]
+print(list)
