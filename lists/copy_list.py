1a4b7f59dba13a9fdf232aacd4790d1f0756c4cf
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-19 23:10:43 +0530

    Add files via upload

---
+++ lists/copy_list.py
@@ -0,0 +1,9 @@
+# using copy() method 
+list1 = [ 1 , 2 , 3 , 4 , 5]
+list2 = list1.copy()
+print(list2)
+
+# use built-in list() method 
+list1 = [1 , 2 , 3 , 4 , 5]
+list2 = list(list1)
+print(list2)
