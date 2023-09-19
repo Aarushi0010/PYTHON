7d2f5d84883938704387f715e2d97c80931862f6
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-19 23:06:32 +0530

    Add files via upload

---
+++ lists/sort_lists.py
@@ -0,0 +1,15 @@
+# sort() method is used to sort the list alphanumerically or 
+# in ascendin(default) or descending order
+
+fruits = ["cherry" , "grapes" , "guava" , "litchi"]
+fruits.sort()
+print(fruits)
+
+list = [50 , 29 , 10 , 34 , 2]
+list.sort()
+print(list)
+
+# sorting in descending order
+list = [ 20 , 49 , 30 , 15 , 99]
+list.sort(reverse = True)
+print(list)
