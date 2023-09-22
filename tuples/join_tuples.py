9f6e513f191428b3a6f921f2b4460e87ab7a475c
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-22 23:07:55 +0530

    Add files via upload

---
+++ tuples/join_tuples.py
@@ -0,0 +1,10 @@
+# 1. using + operator 
+tuple1 = [ 1 , 2 , 3 ]
+tuple2 = [ 4 , 5 , 6 ]
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# 2. using * operator to multiply the content the tuple 
+my_tuple = [1 , 2 , 3 ]
+my_tuple = my_tuple*2
+print (my_tuple)
