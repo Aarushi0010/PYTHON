355d79293fd404b76f8bd59f85f7e178aa93b2fc
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-29 19:22:29 +0530

    Update intro.py

---
+++ sets/intro.py
@@ -0,0 +1,17 @@
+# Sets are unordered , unchangeable and unindexed 
+
+my_set = {1 , 2 , 3}
+print(my_set)
+
+# Duplicate values are ignored
+# True and 1 are considered same value and as duplicates 
+
+# Can contain different data types 
+set1 = { 1 , "aarushi" , True , 2 , 1 , False}
+print(set1)
+print(len(set1))
+print(type(set1))
+
+# use of set() constructor to make a set 
+set2 = set((1 , 2 , "aarushi" , True))
+print(set2)
