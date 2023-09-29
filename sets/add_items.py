b3a22bcc1fa93071912c85fe74ea1da281d7ecdc
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-29 20:02:57 +0530

    Add files via upload

---
+++ sets/add_items.py
@@ -0,0 +1,14 @@
+# 1. using add() method 
+set1 = {1 , 2 , 3 , 4 , 5}
+set1.add(10)
+print(set1)
+
+# 2. using update() method 
+set2 = {6 , 7 , 8 , 9}
+set1.update(set2)
+print(set1)
+
+# 3. add any iterable object
+list1 = ["aarushi" , "japleen" ]
+set1.update(list1)
+print(set1)
