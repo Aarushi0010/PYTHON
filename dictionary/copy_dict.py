e6e01b6294ada7014724daf6165c61ad8a520545
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-30 20:20:48 +0530

    Add files via upload

---
+++ dictionary/copy_dict.py
@@ -0,0 +1,16 @@
+dict1 = {
+    "name" : "aarushi",
+    "year" : 2021,
+    "semester" : 5,
+    "language" : "python",
+    "ID" : 22,
+    "section" : "A1"
+}
+
+# 1. copy() method 
+dict2 = dict1.copy()
+print(dict2)
+
+# 2. dict() method 
+dict2 = dict(dict1)
+print(dict2)
