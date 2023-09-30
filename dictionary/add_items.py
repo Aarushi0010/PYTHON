cf6dc30370649216476c13ae16263df330cfbe44
Aarushi0010 <111633316+Aarushi0010@users.noreply.github.com>
2023-09-30 20:04:42 +0530

    Add files via upload

---
+++ dictionary/add_items.py
@@ -0,0 +1,13 @@
+# 1. adding new key and assignig value to it 
+dict1 = {
+    "name" : "aarushi",
+    "year" : 2021,
+    "semester" : 5,
+    "language" : "python"
+}
+dict1["ID"] = 22
+print(dict1)
+
+# 2. update() method
+dict1.update({"section" : "A1"})
+print(dict1)
